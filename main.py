c0a607943af42d203e1393b609a3c1a91e1b05ea
ohm patel <ohmpatel46@gmail.com>
2021-03-15 06:39:30 +0000

    Made a !inspire(to give a qoute) and a ! fact(to give a fact) command.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import discord
 import os
+import requests
+import json
 
 client=discord.Client()
+
+def get_quote():
+  resp=requests.get("https://zenquotes.io/api/random")
+  json_data=json.loads(resp.text)
+  quote=json_data[0]["q"] + " -" + json_data[0]["a"]
+  return(quote)
+
+def get_fact():
+  resp=requests.get("https://uselessfacts.jsph.pl/random.json?language=en")
+  json_data=json.loads(resp.text)
+  return(json_data["text"])
 
 @client.event
 async def on_ready():
@@ -13,6 +26,14 @@ async def on_message(message):
     return
 
   if message.content.startswith('!hello'):
-    await message.channel.send('Hello!')
+    await message.channel.send('Hello '+message.author.display_name+'!')
+  
+  if message.content.startswith('!inspire'):
+    quote =get_quote()
+    await message.channel.send(quote)
+
+  if message.content.startswith('!fact'):
+    fact=get_fact()
+    await message.channel.send(fact)
 
 client.run(os.getenv('TOKEN'))
